d4db1cc26d9d57a142e9de7f600fa7ad99994a61
sagarsajwan26 <sagarsajwan26@gmail.com>
2025-07-07 01:57:14 +0530

    added cors

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance= axios.create({
-    baseURL:'http://localhost:3000/api/v1',
+    baseURL:'https://todovervel.vercel.app/api/v1',
     withCredentials:true
 
 })
